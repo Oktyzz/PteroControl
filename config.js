2da66db617be92b7e11e464919eccf0ce617ce57
Oktyzz <realredmin@gmail.com>
2024-08-18 10:45:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    inviteSupport: "", // "support server" invite
-    inviteLink: "", // bot invite
-    mongodbConnectionURL: "",
+    inviteSupport: "https://discord.com/invite/TCUHCHfk", // "support server" invite
+    inviteLink: "https://discord.com/invite/TCUHCHfk", // bot invite
+    mongodbConnectionURL: "mongodb+srv://oktzoffc:Oktzoffc@123@cluster0.bx84g40.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     botToken: "",
 }
